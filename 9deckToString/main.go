d2d6eacd0051196e492fa2881fb3dfbb8f8567bb
Keith Lashley <lashleykeith@gmail.com>
2018-07-04 10:15:47 +0900

    Add files via upload

---
+++ 9deckToString/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main(){
+	// cards := newDeck()
+
+	// hand, remainingCards := deal(cards, 5)
+
+	// hand.print()
+	// remainingCards.print()
+	greeting := "Hi there!"
+	fmt.Println([]byte(greeting))
+}
