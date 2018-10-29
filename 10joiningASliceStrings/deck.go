4b80fab8179a710fa11e5513b4508f0add6a53bf
Keith Lashley <lashleykeith@gmail.com>
2018-10-29 10:44:57 +0900

    Add files via upload

---
+++ 10joiningASliceStrings/deck.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+		"fmt"
+		"strings"
+)
+
+//Create a new type of 'deck'
+// which is a slice of strings
+type deck []string
+
+func newDeck() deck{
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts","Clubs"}
+	cardValues := []string{"Ace","Two","Three","Four"}
+
+	for _, suit := range cardSuits{
+		for _, value := range cardValues{
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+
+	return cards
+}
+
+func (d deck) print(){
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
+
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string{
+	return strings.Join([] string (d),",")
+	
+}
+
