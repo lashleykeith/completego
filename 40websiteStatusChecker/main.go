27ff2ad751ac5733b9b76153f07dc4263dc58e5e
Keith Lashley <lashleykeith@gmail.com>
2022-09-28 23:46:56 +0900

    Add files via upload

---
+++ 40websiteStatusChecker/main.go
@@ -0,0 +1,17 @@
+package main
+
+
+
+func main(){
+        links := []string{
+                "http://google.com",
+                "http://facebook.com",
+                "http://stackoverflow.com",
+                "http://golang.com",
+                "http://amazon.com",
+        }
+        for _, link := range links{
+                
+        }
+}
+
---
+++ 42blockingchannel/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+		"fmt"
+		"net/http"
+		)
+
+func main(){
+        links := []string{
+                "http://google.com",
+                "http://facebook.com",
+                "http://stackoverflow.com",
+                "http://golang.com",
+                "http://amazon.com",
+        }
+        
+        c := make(chan string)
+
+        for _, link := range links {
+                go checkLink(link, c)
+        }
+
+        fmt.Println(<- c)
+        fmt.Println(<- c)
+        fmt.Println(<- c)
+        fmt.Println(<- c)
+        fmt.Println(<- c)
+        //fmt.Println(<- c)
+
+}
+
+func checkLink(link string, c chan string){
+	_, err := http.Get(link)
+	if err != nil {
+		fmt.Println(link, "might be down!")
+                c <- "Might be down I think"
+		return
+	}
+
+	fmt.Println(link, " is up!")
+        c <- "Yep it's up"
+}
+
